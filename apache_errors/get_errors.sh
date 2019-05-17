abc821370eb539eba8a966cd9dc59db72b942f50
JoanChirinos <jchirinos3201@gmail.com>
2019-05-17 13:30:03 -0400

    added some files

--- apache_errors/get_errors.sh
+++ apache_errors/get_errors.sh
@@ -1,4 +1,7 @@
 rm errors.txt
 scp root@206.81.1.145:/var/log/apache2/error.log errors.txt
+touch error.log
+scp error.log root@206.81.1.145:/var/log/apache2/.
+rm error.log
 python3 format_errors.py
 open errors.txt
