964acff5cc29c1f87a51be1ed87bb3e40d6f83d2
JoanChirinos <jchirinos3201@gmail.com>
2019-05-17 08:38:00 -0400

    removed some files ig

--- format_errors.py
+++
@@ -1,4 +0,0 @@
-with open('errors.txt') as f:
-	s = f.read()
-with open('errors.txt', 'w') as f:
-	f.write('\n'.join(([i.split(']')[-1] for i in s.split('\n')])))
--- get_errors.sh
+++
@@ -1,4 +0,0 @@
-rm errors.txt
-scp root@206.81.1.145:/var/log/apache2/error.log errors.txt
-python3 format_errors.py
-open errors.txt
