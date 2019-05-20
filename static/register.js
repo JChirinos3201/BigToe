ca94ce00b3a43dda8a6d45dbc12b6b47274a3c9e
AntonDanylenko <adanylenko@stuy.edu>
2019-05-20 14:11:37 -0400

    s

---
+++ static/register.js
@@ -0,0 +1,3 @@
+function showReqs(){
+  document.getElementById("reqs").innerHTML = "Requirements: <ul><li>Least 1 Capital Letter</li><li>Least 1 lowercase letter</li><li>Least 1 number</li><li>Length at least 6</li></ul>"
+}
