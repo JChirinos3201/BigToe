3b0b9680606fc24a76c2d04593275ab67bca30b2
= <jchirinos3201@gmail.com>
2019-05-30 11:47:01 -0400

    I DID IT

--- static/file.js
+++ static/file.js
@@ -1,19 +1,31 @@
-var run_code = function () {
+var run_code = function (filename) {
   var fd = new FormData();
   var code = editor.getValue();
   console.log(code);
   fd.append('code', code);
+  fd.append('filename', filename);
 
   console.log(fd);
 
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      var r = this.responseText;
+      var r = this.response;
       console.log(r);
-      output.setValue(this.responseText);
+
+      var rjson = JSON.parse(r);
+
+      console.log(rjson);
+
+      if (rjson.stderr != "") {
+        output.setValue(rjson.stderr);
+      } else {
+        output.setValue(rjson.stdout);
+      }
+
     }
   }
-  xhttp.open('POST', '/run_code', true);
+  console.log('sending:\n\n' + fd.get('code') + '\n\nand\n\n' + fd.get('filename'));
+  xhttp.open('POST', 'http://159.65.162.204/run', true);
   xhttp.send(fd);
 };
