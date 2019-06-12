2b622cda16811c15d2331db0a0648cd58ab0c59c
= <jchirinos3201@gmail.com>
2019-06-12 00:46:06 -0400

    bois

--- __init__.py
+++ __init__.py
@@ -114,7 +114,8 @@ def get_files(projectId):
     files = [list(i) for i in
              sorted(db.getFiles(projectId), key=lambda x: x[1])]
     for file in files:
-        file[3] = file[3]#.strftime('%I:%M:%S %p | %b %d %Y')
+        file[3] = datetime.datetime.fromisoformat(file[3])\
+            .strftime('%I:%M:%S %p | %b %d %Y')
     return render_template('snippets/project_files.html',
                            projectName=projectName,
                            projectId=projectId,
